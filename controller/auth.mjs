45891ab1b766eff1d3d8c6b60d78636b0d01c284
Hamzaansari15 <hamzaansaree15@gmail.com>
2023-04-20 17:06:49 +0500

    Add Login Api

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -6,49 +6,99 @@ import Jwt from "jsonwebtoken";
 const SecretToken = 'hamzaWeb';
 
 const handleSigninUser = async (req, res) => {
+    const { first_name, last_name, contact, email, password } = await req.body;
     try {
-        const { first_name, last_name, contact, email, password } = await req.body;
-        const user = await userModel.findOne({ email: email })
-        if (user) {
-            res.status(400).send({ message: 'User already exists' });
+        const existUser = await userModel.findOne({ email: email })
+        if (existUser) {
+            res.status(400).send({ message: 'User already Exists' });
             return;
         }
-        try {
+        else {
             if (first_name && last_name && email && contact && password) {
                 const hashPassword = await bcrypt.hash(password, 10)
                 if (hashPassword) {
                     const createUser = await userModel.create({
                         first_name: first_name,
                         last_name: last_name,
-                        email: email,
                         contact: contact,
+                        email: email,
                         password: hashPassword
                     })
                     if (createUser) {
                         console.log(createUser)
-                        const token = await Jwt.sign({})
-                        res.status(201).send({ message: 'User is Created' })
+                        const token = Jwt.sign({
+                            _id: createUser._id,
+                            email: createUser.email,
+                            iat: Math.floor(Date.now() / 1000) - 30,
+                            exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24)
+                        }, SecretToken)
+                        console.log('token', token)
+                        res.cookie('token', token, {
+                            maxAge: 86_400_000,
+                            httpOnly: true
+                        })
+                        res.status(201).send({ message: 'Signin Successfully' })
+                        req.body.token = token;
+                        console.log(req.body)
+                        return;
+                    }
+                    else {
+                        res.status(500).send({ message: 'Server Error' })
                     }
                 }
-
+                else {
+                    res.status(500).send({ message: 'Server Error' })
+                }
+                return;
             }
             else {
-                console.log('jjj')
-                res.send({ message: 'required field is empty' })
+                res.status(400).send({ message: 'Some Fields Are Empty!' })
             }
-        }
-        catch(secondError){
-            console.log(secondError)
-            res.status(500).send({message: 'Server Error 2'})
         }
     }
     catch (error) {
         console.log(error)
-        res.status(500).send({ message: 'Server Error' })
+        res.status(500).send({ message: "Server Error", 'error': error })
     }
-
 
 }
 
 
-export { handleSigninUser }
+const handleLoginUser = async (req, res) => {
+    try {
+        if (email && password) {
+            const { email, password } = req.body;
+            const findUser = await userModel.findOne({ email: email }, 'email password')
+            const match = await bcrypt.compare(password, findUser.password)
+            if (match) {
+                const token = Jwt.sign({
+                    _id: findUser._id,
+                    email: findUser.email,
+                    iat: Math.floor(Date.now() / 1000) - 30,
+                    exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24)
+                }, SecretToken);
+                console.log('token', token)
+                res.cookie('token', token, {
+                    maxAge: 86_400_000,
+                    httpOnly: true
+                })
+                req.body.token = token
+                console.log(req.body)
+                res.status(200).send({message: 'Login Successfully'});
+            }
+        }
+        else{
+            res.status(400).send('Please Filled All Fields');
+        }
+        // console.log(findUser)
+        // res.send('ll')
+
+    }
+    catch (error) {
+        res.status(500).send({ message: 'Server Error', 'Error': error })
+    }
+}
+
+export {
+    handleSigninUser, handleLoginUser
+}
--- routes/user.mjs
+++ routes/user.mjs
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { handleSigninUser } from '../controller/auth.mjs';
+import { handleSigninUser, handleLoginUser } from '../controller/auth.mjs';
 
 const UserRouter = Router();
 UserRouter.post('/signup', handleSigninUser)
-
+UserRouter.post('/login', handleLoginUser)
 
 
 export default UserRouter;
